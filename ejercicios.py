449f3c72a4c05841f17aaf304a342eaa144d9081
aristefcode <stefanixiomaracarlosarias@gmail.com>
2024-05-01 17:53:40 -0500

    Primer ejercicio secuencias

---
+++ ejercicios.py
@@ -0,0 +1,6 @@
+# Crear una lista tupla y rango con numeros del uno al 10
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+rango = range(1, 10)
+
+print(lista, tupla, rango)
